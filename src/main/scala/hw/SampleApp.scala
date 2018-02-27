f9e6283cdcba62644fe0b6059d2b024c2b0ea45d
Elazouzi <35821390+Elazouzi@users.noreply.github.com>
2018-02-27 07:42:35 +0000

    Update SampleApp.scala

--- src/main/scala/hw/SampleApp.scala
+++ src/main/scala/hw/SampleApp.scala
@@ -2,6 +2,6 @@ package main.scala.hw
 import org.apache.spark.SparkContext, org.apache.spark.SparkConf
 object SampleApp {
        def main(args: Array[String]): Unit = {
-         println("Hello, world My name is Dhanush and I am the Best of the Best Today Including faramarz!")
+         println("Hello, world My name is mr Roboto!")
        }
     }
